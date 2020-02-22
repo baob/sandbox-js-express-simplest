c181cc678718399cebccc9ae628b09ed10a71721
baob <coder@onesandthrees.com>
2020-02-22 14:54:38 +0000

    integrate dotenv

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Objective: simplest express
 Reference: [Hello World](https://expressjs.com/en/starter/hello-world.html)
 
 Maybe also [Best practices](https://github.com/Hilzu/node-best-practices)
+
+## Setup
+
+    cp .env.sample .env
+    yarn
+
+## Running
+
+    node app.js
+
--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+require('dotenv').config();
+const message = process.env.MESSAGE || '';
+
+app.get('/', (req, res) => res.send(`Hello World! ${message}`));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
