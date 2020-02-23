204c004266afcc48d0a13874fdaa903eaa6f330e
baob <coder@onesandthrees.com>
2020-02-23 16:04:18 +0000

    fixed bug in eslint/jest config

--- jest-eslint.config.js
+++ jest-eslint.config.js
@@ -3,12 +3,12 @@ const pack = require('./package');
 // See https://jestjs.io/docs/en/configuration
 
 module.exports = {
-    displayName: pack.name,
+    // displayName: pack.name,
     name: pack.name,
     testEnvironment: 'node',
     coveragePathIgnorePatterns: ['/node_modules/'],
 
     runner: 'jest-runner-eslint',
-    // displayName: 'lint',
-    testMatch: ['<rootDir>/**/*.test.js'],
+    displayName: 'lint',
+    testMatch: ['<rootDir>/**/*.js'],
 };
