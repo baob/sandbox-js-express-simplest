246d68ae14a9833a8a423e8350aa42c0338975c4
baob <coder@onesandthrees.com>
2020-02-23 10:45:23 +0000

    test the other route

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,10 +2,26 @@ import request from 'supertest';
 import app from '../app';
 
 describe('app routes', () => {
-    describe('GET /users', () => {
+    describe('GET /', () => {
         it('shows home page', async () => {
             const res = await request(app)
-                .get('/users/')
+                .get('/')
+                .send();
+
+            expect(res.statusCode).toEqual(200);
+            expect(res.headers).toHaveProperty(
+                'content-type',
+                'text/html; charset=utf-8'
+            );
+            expect(res).toHaveProperty('text');
+            expect(res.text).toMatch(/Welcome to Express/);
+        });
+    });
+
+    describe('GET /users', () => {
+        it('shows user response', async () => {
+            const res = await request(app)
+                .get('/users')
                 .send({
                     userId: 1,
                     title: 'test is cool',
