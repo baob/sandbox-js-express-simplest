a98299ee90af3ff916d18c7bad3f7a09d94aa19b
baob <coder@onesandthrees.com>
2020-02-23 21:59:43 +0000

    change displayname of jest tests

--- jest-test.config.js
+++ jest-test.config.js
@@ -3,8 +3,9 @@ const pack = require('./package');
 // See https://jestjs.io/docs/en/configuration
 
 module.exports = {
-    displayName: pack.name,
+    // displayName: pack.name,
     name: pack.name,
+    displayName: 'jest',
     testEnvironment: 'node',
     coveragePathIgnorePatterns: ['/node_modules/'],
 };
